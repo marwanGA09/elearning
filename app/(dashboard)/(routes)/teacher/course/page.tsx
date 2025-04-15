b5dec0ca36884ecbf46c4795edbc7b1dda66f158
Adem Kedir <ademkedir724@gmail.com>
2025-04-15 09:39:26 +0300

    create form element using shadcn-ui, react-hook-form

--- app/(dashboard)/(routes)/teacher/course/page.tsx
+++ app/(dashboard)/(routes)/teacher/course/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 function coursePage() {
-  return <div>course</div>;
+  return (
+    <div>
+      <Link href={'/teacher/create'}>
+        <Button className="p-6">Add course</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default coursePage;
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { z } from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormLabel,
+  FormField,
+  FormItem,
+} from '@/components/ui/form';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const formSchema = z.object({
+  tittle: z.string().min(1, {
+    message: 'Title is required',
+  }),
+});
+
+function CreateCourse() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { tittle: '' },
+  });
+
+  const { isSubmitting, isLoading, isValid } = form.formState;
+
+  const onSubmit = (value: z.infer<typeof formSchema>) => {
+    console.log({ value });
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">Name your course</h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your course? Don&apos;t worry, you can
+          change this later{' '}
+        </p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="tittle"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="e.g. 'Web Development'"
+                      {...field}
+                      disabled={isSubmitting}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is the name of your course.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href={'/'}>
+                <Button type="button" variant={'ghost'}>
+                  Cancel
+                </Button>
+              </Link>
+
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateCourse;
